13a3307aaedc82c85bc22c78bfa59b0404868a87
Arely Rizo <arelyruiz50700@gmail.com>
2023-11-27 14:59:49 -0600

    ejercicio primera correccion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+
+
+
+
+const baseEndpoint = 'https://api.github.com'; // el nombre de la variable no es muy legible 
+const usersEndpoint = `${baseEndpoint}/users`;// esta linea es algo irrelevante para el codigo 
+const $n = document.querySelector('#name'); // Cambio en el selector quitando un punto y agrgando #name
+const $b = document.querySelector('#blog'); // se borro un punto y se  agrego un # al blog 
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); // Se crea una nueva const para poder pasar la respuesta a json
+    console.log(data);
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`; 
 }
 
 displayUser('stolinski').catch(handleError);
